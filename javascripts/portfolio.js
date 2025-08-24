ef8fb97abdb9e9b87b09a423f845eaff6c1a2fd4
rené <ren@mmo>
2025-08-24 22:07:46 +0200

    portfolio list done

--- javascripts/portfolio.js
+++ javascripts/portfolio.js
@@ -1,7 +1,26 @@
 import {fetchHeader} from "./utils.js";  
 
+
+
 const setup = async () => {
     await fetchHeader();
+    setupEventListeners();
+}
+
+const linkProjects = () => {   
+    const projects = document.querySelectorAll(".project"); 
+    projects.forEach(project => {
+        project.addEventListener("click", () => {
+            const link = project.getAttribute("data-link"); 
+            if (link) window.open(link); 
+            else alert("Nothing is linked");
+        })
+    });
+}
+
+
+const setupEventListeners = () => {
+    linkProjects();
 }
 
 document.addEventListener("DOMContentLoaded", setup); 
