a58ff14f6eea42937ed37a07ab9baf65c78b9cbc
rené <ren@mmo>
2025-08-28 23:09:24 +0200

    ugly cv done

---
+++ html/c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct cv{
+    int yaer; 
+    char[2] *studies; 
+}
+
+void printCV(struct cv struct_cv) {
+    int dec; 
+    struct_cv->yaer +=1;
+    dec+=1; 
+    if (dec == 1) {
+        for (int i = 0 ; i < 10 ; i++){ 
+            dec = "dec"
+        }
+    }
+}
